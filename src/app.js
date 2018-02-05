ec536c55db2b3c9634699e1e7401415bab485dcd
benlloydjones <benlloydjones@gmail.com>
2018-02-05 20:58:34 +0000

    video ids displaying on front end

--- src/app.js
+++ src/app.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Axios from 'axios';
 
 import '../node_modules/bulma/bulma.sass';
 
 class App extends React.Component {
 
+  state = {
+    videos: null
+  }
+
+  componentDidMount() {
+    Axios
+      .get('/api/youTube/search')
+      .then(res => {
+        this.setState({ videos: res.data });
+        console.log(res.data);
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
-      <h1 className="title is-1">Hello World</h1>
+      <div className="container">
+        <h1 className="title is-1">Hello, world!</h1>
+        <ul>
+          {this.state.videos && this.state.videos.items.map(video => {
+            return (<li key={video.id.videoId}>
+              <p>{video.id.videoId}</p>
+            </li>);
+          })}
+        </ul>
+      </div>
+
     );
   }
 
